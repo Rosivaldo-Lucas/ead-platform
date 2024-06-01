4efcbd567500d9e0a3f0f07c31cbdacb99a7738d
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-06-01 09:28:18 -0300

    commit inicial do microsservice authuser-api.

---
+++ authuser-api/src/main/java/com/rosivaldolucas/ead/authuser_api/AuthuserApiApplication.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.ead.authuser_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuserApiApplication.class, args);
+	}
+
+}
---
+++ authuser-api/src/test/java/com/rosivaldolucas/ead/authuser_api/AuthuserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.ead.authuser_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
