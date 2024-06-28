bbeca759df8db370152b2726d898606c0e41a884
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-06-27 22:46:33 -0300

    (course-api)feat: adiciona validacoes e comunicacao entre course-api e authuser-api para verificacoes do usuario que esta se inscrevendo em um curso

--- course-api/src/main/java/com/rosivaldolucas/ead/course_api/controllers/CourseUserController.java
+++ course-api/src/main/java/com/rosivaldolucas/ead/course_api/controllers/CourseUserController.java
@@ -1,8 +1,9 @@
 package com.rosivaldolucas.ead.course_api.controllers;
 
-import com.rosivaldolucas.ead.course_api.clients.CourseClient;
+import com.rosivaldolucas.ead.course_api.clients.AuthUserClient;
 import com.rosivaldolucas.ead.course_api.dtos.SubscriptionDTO;
 import com.rosivaldolucas.ead.course_api.dtos.UserDTO;
+import com.rosivaldolucas.ead.course_api.enums.UserStatus;
 import com.rosivaldolucas.ead.course_api.models.Course;
 import com.rosivaldolucas.ead.course_api.models.CourseUser;
 import com.rosivaldolucas.ead.course_api.services.CourseService;
@@ -16,8 +17,10 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.validation.Valid;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,7 +30,7 @@ import java.util.UUID;
 public class CourseUserController {
 
   @Autowired
-  private CourseClient courseClient;
+  private AuthUserClient authUserClient;
 
   @Autowired
   private CourseService courseService;
@@ -35,11 +38,21 @@ public class CourseUserController {
   @Autowired
   private CourseUserService courseUserService;
 
+  @GetMapping("/courses/{courseId}/users")
+  public ResponseEntity<Page<UserDTO>> getAllUsersByCourse(
+          @PathVariable UUID courseId,
+          @PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable
+  ) {
+    return ResponseEntity.status(HttpStatus.OK).body(this.authUserClient.getAllUsersByCourse(courseId, pageable));
+  }
+
   @PostMapping("/courses/{courseId}/users/subscription")
   public ResponseEntity<?> saveSubscriptionInCourse(
           @PathVariable UUID courseId,
           @RequestBody @Valid SubscriptionDTO subscriptionDTO
   ) {
+    ResponseEntity<UserDTO> responseUser;
+
     Optional<Course> courseOptional = this.courseService.findById(courseId);
 
     if (courseOptional.isEmpty()) {
@@ -50,19 +63,21 @@ public class CourseUserController {
       return ResponseEntity.status(HttpStatus.CONFLICT).body("Subscription already exists");
     }
 
-    // TODO: verificação user
+    try {
+      responseUser = this.authUserClient.getOneUserById(subscriptionDTO.getUserId());
+
+      if (Objects.requireNonNull(responseUser.getBody()).getUserStatus().equals(UserStatus.BLOCKED)) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("User is blocked");
+      }
+    } catch (HttpStatusCodeException ex) {
+      if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+      }
+    }
 
     CourseUser courseUser = courseUserService.save(courseOptional.get().converterToCourseUser(subscriptionDTO.getUserId()));
 
-    return ResponseEntity.status(HttpStatus.CREATED).body("Subscription created successfully");
-  }
-
-  @GetMapping("/courses/{courseId}/users")
-  public ResponseEntity<Page<UserDTO>> getAllUsersByCourse(
-          @PathVariable UUID courseId,
-          @PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable
-  ) {
-    return ResponseEntity.status(HttpStatus.OK).body(this.courseClient.getAllUsersByCourse(courseId, pageable));
+    return ResponseEntity.status(HttpStatus.CREATED).body(courseUser);
   }
 
 }
--- course-api/src/main/java/com/rosivaldolucas/ead/course_api/services/UtilsService.java
+++ course-api/src/main/java/com/rosivaldolucas/ead/course_api/services/UtilsService.java
@@ -1,6 +1,5 @@
 package com.rosivaldolucas.ead.course_api.services;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +8,11 @@ import java.util.UUID;
 @Service
 public class UtilsService {
 
-    @Value("${ead.api.url.authuser}")
-    private String REQUEST_URI;
-
-    public String createUrl(UUID userId, Pageable pageable) {
-        return REQUEST_URI +
-                "/courses?userId=" + userId +
-                "&page=" + pageable.getPageNumber() +
-                "&size=" + pageable.getPageSize() +
-                "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
-    }
+  public String createUrl(UUID userId, Pageable pageable) {
+    return "/courses?userId=" + userId +
+            "&page=" + pageable.getPageNumber() +
+            "&size=" + pageable.getPageSize() +
+            "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+  }
 
 }
