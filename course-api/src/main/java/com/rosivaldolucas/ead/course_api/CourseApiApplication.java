829b523206515eccfba7c39b1218994439a0ab32
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-07-01 21:40:09 -0300

    course-api:feat: adiciona service registry

--- course-api/src/main/java/com/rosivaldolucas/ead/course_api/CourseApiApplication.java
+++ course-api/src/main/java/com/rosivaldolucas/ead/course_api/CourseApiApplication.java
@@ -2,8 +2,10 @@ package com.rosivaldolucas.ead.course_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CourseApiApplication {
 
 	public static void main(String[] args) {
